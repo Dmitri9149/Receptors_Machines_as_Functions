39d1a333b251a6391d407f0b9a124d73b1e49333
Dmitri9149 <me@notset.com>
2022-04-06 16:08:34 +0300

    some development

--- Genes_1.hs
+++ Genes_1.hs
@@ -1,6 +1,8 @@
 -- here I try to use Functor class to clarify the transformations 
 -- are actually functors, to be more specific : isomorphisms 
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+import Control.Arrow
+import Data.Functor.Contravariant
 {-# HLINT ignore "Use camelCase" #-}
 -- import Data.Map as Map
 
@@ -53,7 +55,7 @@ code_transformer_DNA_to_RNA = map from_DNA_to_RNA
 
 
 -- package of RNA_Code
-type RNA_Package = [RNA_Code]
+type RNA_Packages = [RNA_Code]
 
 -- this function transform from DNA_Packge to RNA_Package 
 package_transformer_DNA_RNA :: DNA_Packages -> RNA_Packages
@@ -92,13 +94,13 @@ code_transformer_RNA_Protein = undefined
 -- we will use mostly machines which work as values of Session Types 
 
 
-type ProteinMachines = [ProteinCode]
+newtype ProteinMachines = ProteinMachines {fromCode :: [ProteinCode]}
 type ProteinPackages = ProteinMachines
 
 -- this function transform from RNA_Packages to Protein_Packages (or ProteinMachines)
 
 package_transformer_RNA_Proteins :: RNA_Packages -> ProteinMachines
-package_transformer_RNA_Proteins = map code_transformer_RNA_Protein
+package_transformer_RNA_Proteins rna = ProteinMachines {fromCode = map code_transformer_RNA_Protein rna }
 
 -- let us consider only one class of Protein machines : which are a values of Session Types 
 -- let us consider some enumerable state a and one to one function from a to Promoters
@@ -112,23 +114,61 @@ our_Genome = undefined
 
 state_to_machines :: a -> ProteinMachines
 state_to_machines =
-  package_transformer_DNA_RNA
-  .
   package_transformer_RNA_Proteins
+  .
+  package_transformer_DNA_RNA
   .
   from_enum_to_packages our_Genome
   .
   state_to_promoters
 
-state_to_machines1 :: a -> ProteinMachines
-state_to_machines1 = 
-  package_transformer_DNA_RNA
-  (flip .)
-  package_transformer_RNA_Proteins
-  (flip .)
-  from_enum_to_packages our_Genome
-  (flip .)
-  state_to_promoters
 
+-- if we write from left to right :  from state a to the promoters to the ... ProteinsPackages 
+state_to_machines' :: a -> ProteinMachines
+state_to_machines' = 
+  state_to_promoters 
+  >>> 
+  from_enum_to_packages our_Genome
+  >>>
+  package_transformer_DNA_RNA
+  >>>
+  package_transformer_RNA_Proteins
+
+-- because all the maps were injections there is to be reverse function : 
+machines_to_state :: ProteinMachines -> a 
+machines_to_state = undefined 
+
+-- let us consider the scenarion : we have agents which are in some state from some 
+-- Enumerable a ; for every i from a there is unique code which coding the 
+-- unique protein machine i ; this machine is value of Sesion Type -> actually 
+-- our state from a is in one to one relation to the protein machine i 
+-- val of Session Type of agent i can interact with val of Session Type of agent j 
+-- the values (proteins machines) implement the interaction 
+-- the implementation will be done in separate module in future 
+-- insimplest form : interaction between (protein machine i ) and (protein machine j ) 
+-- is given just by function : interaction :: (a , a) -> (a , a) 
+interaction_1 :: (a,a) -> (a,a)
+interaction_1 = undefined 
+
+-- we can forget abou the DNA , RNA and the code and think about a new type 
+-- ProteinMacnines' which is just 
+newtype ProteinMachines' a = ProteinMachines' { stateToMachines :: a -> ProteinMachines } 
+-- we see this is a Contravariant functor 
+instance Contravariant ProteinMachines' where 
+  contramap f pm@ProteinMachines'{stateToMachines = stm} 
+    = ProteinMachines' { stateToMachines = stm . f }
+
+-- we can easily lift the interaction_1 to ProteinMachines and to ProteinMachines'
+
+interaction_11 :: (a,a) -> ((a,a) -> (a,a)) -> (a -> ProteinMachines) -> (ProteinMachines, ProteinMachines)
+interaction_11 (m1,m2) interact state_to_machines = 
+  let (res1, res2) = interact (m1,m2) in (state_to_machines res1, state_to_machines res2)
+
+-- if we remind about 'biology' about Promoters , DAN and RNA code there are much more steps 
+-- the first step is interacting machines will generate not immediatelly the 
+-- next pair machines but the pair (prom1 :: Promoters , prom2 :: Promoters) 
+
+
+-- continuation_in_promoters :: (ProteinMachines, ProteinMachines) -> (Promoters, Promoters)
 
 
