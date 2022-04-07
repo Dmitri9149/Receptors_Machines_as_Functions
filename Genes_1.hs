2c87c962a732348e8de66b34de406dfe5bd030ae
Dmitri9149 <me@notset.com>
2022-04-07 14:08:46 +0300

    some comments

--- Genes_1.hs
+++ Genes_1.hs
@@ -1,49 +1,71 @@
--- here I try to use Functor class to clarify the transformations 
--- are actually functors, to be more specific : isomorphisms 
+-- one more trial to 'idiomatically' describe the Genes
+-- interplay between the code and processes ( machines) 
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-import Control.Arrow
-import Data.Functor.Contravariant
+import Control.Arrow ( (>>>) )
+import Data.Functor.Contravariant ( Contravariant(contramap) )
 
-{-# HLINT ignore "Use camelCase" #-}
--- import Data.Map as Map
-
--- one of my attempts to make a right type system to 
--- such things are DNA / Genes ; RNA / Proteins 
+{-# HLINT ignore "Use camelCase" #-} 
 
 -- aplhabet of DNA 
 -- every gene is string in A, T, C, G (nucleotides) alphabet
 data DNA_Alphabet = A | T | C | G
 
--- these are the regulatory elements , which are used to 
--- transcribe subsets of genes 
--- a promotor is list in DNA_Alphabet
-type Promoters = [DNA_Alphabet]
--- a gene is list in DNA_Alphabet 
+-- DNA is long string in DNA alphabet 
+-- some of it substrings are 'genes' 
+-- this substrings code proteins : elementary processes 
+-- of the system 
+-- the Genes is just a type synonim for DNA_Code 
+-- we Genes link us with biology and DNA_Code is referense to 
+-- Computer science
 type DNA_Code = [DNA_Alphabet]
-type Genes = DNA_Code
--- we are dealing not with a single genes , but with subsets of 
--- genes : DNA_Packages and  Genome is actually the map from promoters to 
--- collection (here list) of genes 
--- Genome is like main memory which has complex organization 
+type Genes = DNA_Code 
+
+
+-- a working 'unit' of Genome is not a gene , but some subset of genes 
+-- it is same way as we usually never use only one module in programming, 
+-- but we use a package of modules to get the functionality we are in need. 
+-- 
+-- promoters are the regulatory elements, substrings of DNA, which are used to 
+-- transcribe subsets of genes 
+-- a promotor is also list in DNA_Alphabet 
+-- special proteins : transcription factors recognize the promoters and 
+-- work as quiry in a data base : get a subset of records
+type Promoters = [DNA_Alphabet]
+
+-- we are dealing not with a single gene, but with subsets of 
+-- genes : DNA_Packages 
+-- this is code level : we can think about the subsets as 
+-- as modules written in some programming language and collected 
+-- to a package 
 type DNA_Packages = [DNA_Code]
 
+-- Genome is actually the map from promoters to 
+-- collection (here list) of genes 
+-- Genome is like main memory which has complex organization 
+-- 
 -- the function is to be injective !
 newtype Genome = Genome { from_enum_to_packages:: Promoters  -> DNA_Packages }
 
 -- RNA is like cache memory , intermediate memory 
+-- but we also can think about it as a compiled code , 
+-- where genes correspond to uncompiked code 
+-- only that part of code which is in big need now for cell is 
+-- transcribed from DNA to RNA and saved as cache memory 
+-- from which we will spawn protein machines (run compiled packages)
 -- gene in DNA alphabet is transcribed firstly to RNA alphadet 
 -- A' is a biochemical modification to A , C' to C , G' to G and U' 
 -- correpond to T ( at DNA level)
 data RNA_Alphabet = A' | C' | G' | U'
 type RNA_Code = [RNA_Alphabet]
 
+-- this are functions from 'biology' 
 from_DNA_to_RNA :: DNA_Alphabet -> RNA_Alphabet
 from_DNA_to_RNA x = case x of
   A -> A'
   C -> C'
   G -> G'
   T -> U'
-
+-- this is well known from biology 
 from_RNA_to_DNA :: RNA_Alphabet -> DNA_Alphabet
 from_RNA_to_DNA r = case r of
   A' -> A
@@ -51,24 +73,30 @@ from_RNA_to_DNA r = case r of
   G' -> G
   U' -> T
 
+-- elements of RNA_Code are in one to one correspondence with DNA_Code
 code_transformer_DNA_to_RNA :: DNA_Code -> RNA_Code
 code_transformer_DNA_to_RNA = map from_DNA_to_RNA
 
 
--- package of RNA_Code
+-- there is isomorphism between RNA_Packages and DNA_Packages
+-- to every DNA_Package correspond RNA_Package 
+-- and every 'gene' in a DNA_Package compiles to some RNA element in the 
+-- RNA_Package 
 type RNA_Packages = [RNA_Code]
 
 -- this function transform from DNA_Packge to RNA_Package 
 package_transformer_DNA_RNA :: DNA_Packages -> RNA_Packages
 package_transformer_DNA_RNA = map code_transformer_DNA_to_RNA
 
-
-
-
--- DNA and RNA molecules are like an uncompiled code 
--- a biological machine is made from proteins , which are the 
--- processes (compiled code) 
--- for simplicity we will assume one RNA molecule correspond to one protein 
+-- RNA_Packages correspond to cache memory / compiled code 
+-- from this code packages we can run (spawn) mature processes : 
+-- protein machines 
+-- firstly we translate every RNA 'gene element' to string 
+-- in amino acids alphabet 
+-- then we fold the strings in 3-D structured molecules 
+-- proteins / protein subunits 
+-- then we combine the folded element in even more high ordered 
+-- structure : protein machine : actuall process units of the system 
 
 -- the letters in the alphabet correspond to 
 -- amino acids which are the monomers that make up proteins
@@ -78,25 +106,25 @@ package_transformer_DNA_RNA = map code_transformer_DNA_to_RNA
 data ProteinsAlphabet = A'' | C'' | D'' | E'' | F'' | G'' | H'' | I'' | K''
                         | L'' | M'' | N'' | P'' | Q'' | R'' | S'' | T'' | V'' | W'' |Y''
 
--- there is function which can transform RNACode ([RNAs]) to 
--- string in ProteinCode (string in [ProteinsAlphabet])
-
+-- we have the same one to one structure as for RNA_Code and DNA_Code 
+-- we have the chain of isomorphisms 
 type ProteinCode = [ProteinsAlphabet]
 
--- there is function which transform from RNA code to Protein code 
+-- there is function which can transform RNA_Code ([RNAs]) to 
+-- string in ProteinCode (string in [ProteinsAlphabet])
+-- it is well known transformation from 'biology'
 code_transformer_RNA_Protein :: RNA_Code -> ProteinCode
 code_transformer_RNA_Protein = undefined
 
 -- the proteins are folded to the final 3-D structure in Golgi apparatus
 -- this is like compilation and spawning  a program ( like runhaskell command)
--- we use newtype to wrap the Protein string to reflects the compilation / folding 
--- and we reflect the fact the active unit is now a single protein 
+-- we use newtype to wrap the Protein string to reflects the folding / spawning
+-- and we reflect the fact the active unit is not a single protein 
 --  but it is usually a list of proteins collected together to make a 'machine'
 -- we will use mostly machines which work as values of Session Types 
 
-
-newtype ProteinMachines = ProteinMachines {fromCode :: [ProteinCode]}
-type ProteinPackages = ProteinMachines
+newtype ProteinPackages = ProteinMachines {fromCode :: [ProteinCode]}
+type ProteinMachines = ProteinPackages
 
 -- this function transform from RNA_Packages to Protein_Packages (or ProteinMachines)
 
